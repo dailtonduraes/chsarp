e741fa426fc74a0e50f9fb9f068fb3d18ad94607
DailtonDurães <dailtonsd10@gmail.com>
2020-08-25 12:28:12 -0300

    Composition

---
+++ Composition2/Composition2/Entities/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Composition2.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+
+}
---
+++ Composition2/Composition2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Composition2.Entities;
+
+namespace Composition2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment com1 = new Comment("Have a nice trip!");
+            Comment com2 = new Comment("Wowww that's awesome!");
+            Comment com3 = new Comment("Nice :DDD");
+
+            Post p1 = new Post(
+                DateTime.Parse("21/03/2020 13:05:50"),
+                "Traveling to China",
+                "I'm going visit this country!",
+                12
+                );
+
+            p1.AddComment(com1);
+            p1.AddComment(com2);
+            p1.AddComment(com3);
+
+            Console.WriteLine(p1);
+        }
+    }
+}
